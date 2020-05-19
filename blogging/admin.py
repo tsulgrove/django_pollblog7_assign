a664194a82346d3890546a16b6bc5df6c35de485
Tom <tsulgrove@gmail.com>
2020-05-19 16:37:04 -0700

    added category to admin posts

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    
+    model = Category.posts.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    
+    exclude = ('posts', )
+
+
+class PostAdmin(admin.ModelAdmin):
+    
+    inlines = [
+        CategoryInline,
+    ]
+ 
+    
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
